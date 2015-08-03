05e9dd9fffb44bce1951523be72e016219841539
MKS-students <mks.students@hackreactor.com>
2015-08-03 13:45:59 -0500

    Completed Taxonomy

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,11 @@
 var Bee = function() {
+ this.age = 5;
+ this.color = 'yellow';
+ this.job = 'keep on growing';
+ Bee.prototype.constructor = Bee
 };
 
+Bee.prototype = new Grub;
+
+//var bee1 = Bee()
+//bee1.prototype.constructor
--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,12 @@
 var ForagerBee = function() {
+  this.age = 10;
+  this.job = 'find pollen';
+  this.canFly = true;
+  this.treasureChest = [];
+  ForagerBee.prototype.constructor = ForagerBee;
 };
 
+ForagerBee.prototype = new Bee;
+ForagerBee.prototype.forage = function(treasure){
+  this.treasureChest.push(treasure);
+};
