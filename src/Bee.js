3df43d98031a30399aadb675179683c7e552f0f2
MKS-students <mks.students@hackreactor.com>
2015-08-04 10:57:05 -0500

    Used psuedoclassical Object.create to finish

--- src/Bee.js
+++ src/Bee.js
@@ -1,11 +1,15 @@
 var Bee = function() {
- this.age = 5;
- this.color = 'yellow';
- this.job = 'keep on growing';
- Bee.prototype.constructor = Bee
+  Grub.call(this);
+  this.age = 5;
+  this.color = 'yellow';
+  this.job = 'keep on growing';
 };
+  Bee.prototype = Object.create(Grub.prototype)
+  Bee.prototype.constructor = Bee;
 
-Bee.prototype = new Grub;
+
+
+// Bee.prototype = new Grub;
 
 //var bee1 = Bee()
 //bee1.prototype.constructor
